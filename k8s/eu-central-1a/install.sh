bbeb951a2f4b3c82431b9da4ae1220761f027e40
Keegan Ferrando <fauweh@unzane.com>
2019-03-18 14:04:55 -0700

    Restructuring remaining zones to match MDN vars

--- k8s/eu-central-1a/install.sh
+++ k8s/eu-central-1a/install.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-kops create cluster ${KOPS_NAME} \
+kops create cluster --name=${KOPS_CLUSTER} \
     --authorization=RBAC \
     --cloud aws \
-    --kubernetes-version=${KOPS_K8S_VERSION} \
+    --kubernetes-version=${KUBERNETES_VERSION} \
+    --master-count=${KOPS_MASTER_COUNT} \
     --master-size=${KOPS_MASTER_SIZE} \
     --master-volume-size=${KOPS_MASTER_VOLUME_SIZE_GB} \
     --master-zones=${KOPS_MASTER_ZONES} \
-    --networking=calico \
+    --networking=${KOPS_NETWORKING} \
     --node-count=${KOPS_NODE_COUNT} \
     --node-size=${KOPS_NODE_SIZE} \
     --node-volume-size=${KOPS_NODE_VOLUME_SIZE_GB} \
     --ssh-access=${KOPS_SSH_IP} \
-    --ssh-public-key=${KOPS_PUBLIC_KEY} \
+    --ssh-public-key=${KOPS_SSH_PUB_KEY:-~/.ssh/id_rsa.pub} \
     --target=terraform \
     --vpc=${KOPS_VPC_ID} \
     --zones=${KOPS_ZONES} \
