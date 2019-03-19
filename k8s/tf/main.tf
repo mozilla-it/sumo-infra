bacafbd383fe01b5232902b41d6e9b859d3761a2
eziegenhorn <eric@ziegenhorns.com>
2019-03-19 13:35:00 -0500

    Disable encryption on kops state bucket

--- k8s/tf/main.tf
+++ k8s/tf/main.tf
@@ -51,15 +51,6 @@ resource "aws_s3_bucket" "sumo-kops-state" {
     enabled = true
   }
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = "${aws_kms_key.s3_key.arn}"
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-
   tags = {
     "Environment" = "${var.environment}"
     "app"         = "sumo"
