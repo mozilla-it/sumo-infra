9d0effb7cba59641c21de7ceb7fbcd3827684536
eziegenhorn <eric@ziegenhorns.com>
2019-04-15 14:49:23 -0500

    Remove unused kms key for s3 buckets

--- k8s/tf/00_aws-vpc/us-west-2/main.tf
+++ k8s/tf/00_aws-vpc/us-west-2/main.tf
@@ -37,11 +37,6 @@ module "vpc" {
   }
 }
 
-resource "aws_kms_key" "s3_key" {
-  description             = "This key is used to encrypt bucket objects"
-  deletion_window_in_days = 10
-}
-
 resource "aws_s3_bucket" "sumo-kops-state" {
   bucket = "sumo-kops-state-095732026120"
   acl    = "private"
