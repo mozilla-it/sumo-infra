620ea2922468fbcdb7f46930fa208194c8792dc7
eziegenhorn <eric@ziegenhorns.com>
2019-04-19 14:20:32 -0500

    Make vpc creation use more variables and provide more outputs for use later

--- k8s/tf/00_aws-vpc/us-west-2/main.tf
+++ k8s/tf/00_aws-vpc/us-west-2/main.tf
@@ -28,8 +28,8 @@ module "vpc" {
   enable_nat_gateway = true
   single_nat_gateway = true
 
-  private_subnets = ["10.0.0.0/19", "10.0.64.0/19", "10.0.128.0/19"]
-  public_subnets  = ["10.0.32.0/19", "10.0.96.0/19", "10.0.160.0/19"]
+  private_subnets = "${var.private_subnets}"
+  public_subnets = "${var.public_subnets}"
 
   tags = {
     "Environment" = "${var.environment}"
--- k8s/tf/00_aws-vpc/us-west-2/outputs.tf
+++ k8s/tf/00_aws-vpc/us-west-2/outputs.tf
@@ -3,9 +3,13 @@ output "vpc_id" {
 }
 
 output "private_subnets" {
-  value = "${module.vpc.private_subnets}"
+    value = "${module.vpc.private_subnets}"
 }
 
+#output "cidr_block" {
+#  value = "${module.vpc.cidr_block}"
+#}
+
 output "public_subnets" {
-  value = "${module.vpc.public_subnets}"
+    value = "${module.vpc.public_subnets}"
 }
