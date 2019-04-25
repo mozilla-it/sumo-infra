4d06eba347ce776849826e8b8fee2a104e6ce146
eziegenhorn <eric@ziegenhorns.com>
2019-04-25 11:32:38 -0500

    Use correct vpc cidr and updated DB size

--- k8s/tf/60_db_redis/main.tf
+++ k8s/tf/60_db_redis/main.tf
@@ -28,9 +28,8 @@ module "mysql-dev" {
   mysql_security_group_name   = "sumo_rds_sg_dev"
   mysql_storage_gb            = 250
   mysql_storage_type          = "gp2"
-
-  vpc_id   = "${data.terraform_remote_state.vpc.vpc_id}"
-  vpc_cidr = "${data.aws_subnet.database.*.cidr_block[0]}"
+  vpc_id                      = "${data.terraform_remote_state.vpc.vpc_id}"
+  vpc_cidr                    = "${data.terraform_remote_state.vpc.cidr_block}"
 }
 
 /*
@@ -48,25 +47,25 @@ module "mysql-stage" {
     mysql_storage_gb = 250
     mysql_storage_type = "gp2"
     vpc_id = "${data.terraform_remote_state.vpc.vpc_id}"
-    vpc_cidr = "${join(", ", data.aws_subnet.database.*.cidr_block)}"
+    vpc_cidr                    = "${data.terraform_remote_state.vpc.cidr_block}"
 }
 */
 
 module "mysql-prod" {
-  source = "rds-multi-az"
+  source    = "rds-multi-az"
+  mysql_env = "prod"
 
   # DBName must begin with a letter and contain only alphanumeric characters
-  mysql_env                   = "prod"
   mysql_db_name               = "sumo_prod"
   mysql_username              = "root"
   mysql_password              = "${var.mysql_prod_password}"
   mysql_identifier            = "sumo"
-  mysql_instance_class        = "db.m4.2xlarge"
+  mysql_instance_class        = "db.m5.4xlarge"
   mysql_backup_retention_days = 7
   mysql_security_group_name   = "sumo_rds_sg_prod"
   mysql_storage_gb            = 250
   mysql_storage_type          = "gp2"
   subnets                     = "${join(",", data.aws_subnet_ids.database.ids)}"
   vpc_id                      = "${data.terraform_remote_state.vpc.vpc_id}"
-  vpc_cidr                    = "${data.aws_subnet.database.*.cidr_block[0]}"
+  vpc_cidr                    = "${data.terraform_remote_state.vpc.cidr_block}"
 }
