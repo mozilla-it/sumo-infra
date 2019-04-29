803a8ae4c7ce7f59cc0439ae7021129238a4d6b1
eziegenhorn <eric@ziegenhorns.com>
2019-04-29 15:40:43 -0500

    Fix duplicated variables

--- k8s/tf/50_cdn/static_media_s3/static_media_s3.tf
+++ k8s/tf/50_cdn/static_media_s3/static_media_s3.tf
@@ -1,19 +1,3 @@
-variable "bucket_name" {}
-variable "iam_policy_name" {}
-variable "logging_bucket_id" {}
-variable "logging_prefix" {}
-variable "region" {}
-variable "hosted-zone-id-defs" {
-  # See: https://docs.aws.amazon.com/general/latest/gr/rande.html#s3_website_region_endpoints
-  type = "map"
-
-  default = {
-    us-east-1 = "Z3AQBSTGFYJSTF"
-    us-west-2 = "Z3BJ6K6RIION7M"
-  }
-}
-
-
 resource "aws_s3_bucket" "sumo-static-media" {
   bucket = "${var.bucket_name}"
   region = "${var.region}"
