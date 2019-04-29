b6008454769545ef16fe66e6796ca764dba05bee
eziegenhorn <eric@ziegenhorns.com>
2019-04-29 16:14:55 -0500

    Show subnet groups in vpc output

--- k8s/tf/00_aws-vpc/us-west-2/outputs.tf
+++ k8s/tf/00_aws-vpc/us-west-2/outputs.tf
@@ -13,3 +13,11 @@ output "cidr_block" {
 output "public_subnets" {
   value = "${module.vpc.public_subnets}"
 }
+
+output "db_subnet_group" {
+  value = "${module.vpc.database_subnet_group}"
+}
+
+output "redis_subnet_group" {
+  value = "${module.vpc.elasticache_subnet_group}"
+}
