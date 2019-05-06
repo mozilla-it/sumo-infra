727ada5d19d4fcdc6bd5819edee62e96b02a21bf
Keegan Ferrando :fauweh <kferrando@mozilla.com>
2019-05-06 09:18:52 -0700

    Lower ASG grace period, ditching jenkins config.sh

--- k8s/tf/99_jenkins/main.tf
+++ k8s/tf/99_jenkins/main.tf
@@ -161,7 +161,7 @@ resource "aws_autoscaling_group" "ci" {
   max_size                  = "1"
   min_size                  = "1"
   desired_capacity          = "1"
-  health_check_grace_period = 1800
+  health_check_grace_period = 300
 
   # Less than ideal but the initial sync takes such a long time
   health_check_type    = "EC2"
