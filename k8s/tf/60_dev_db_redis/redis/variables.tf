bc2e7a075dd25c390559dcaf8b74058738822392
eziegenhorn <eric@ziegenhorns.com>
2019-05-08 10:50:35 -0500

    Create dev redis instance submodule

---
+++ k8s/tf/60_dev_db_redis/redis/variables.tf
@@ -0,0 +1,25 @@
+variable "redis_name" {}
+
+variable "redis_node_size" {}
+
+variable "redis_port" {
+  default = 6379
+}
+
+variable "redis_num_nodes" {
+  default = 1
+}
+
+variable "redis_param_group" {
+  default = "default.redis3.2"
+}
+
+variable "redis_engine_version" {
+  default = "3.2.4"
+}
+
+variable "subnets" {}
+
+variable "nodes_security_groups" {}
+
+variable "redis_subnet" {}
