2388283ea1e626949b94555c77b46c67e3682319
Keegan Ferrando :fauweh <kferrando@mozilla.com>
2019-05-28 14:07:09 -0700

    Reverting ELB creation method

--- k8s/install/post-install.sh
+++ k8s/install/post-install.sh
@@ -28,152 +28,6 @@ set_tf_resource_name() {
     export TF_RESOURCE_NAME=$(echo ${KOPS_CLUSTER_NAME} | tr "." "-")
 }
 
-apply_ingress_controller_tf() {
-    set_tf_resource_name
-    # https://aws.amazon.com/blogs/opensource/kubernetes-ingress-aws-alb-ingress-controller/
-    # https://kubernetes-sigs.github.io/aws-alb-ingress-controller/
-    echo "Generating ./out/terraform/aws_alb_ingress_controller.tf"
-    cat <<BASHEOF > ./out/terraform/aws_alb_ingress_controller.tf
-# This file is generated via post-install.sh
-resource "aws_iam_policy" "nodes-${TF_RESOURCE_NAME}-aws-alb-ingress-controller" {
-    name        = "nodes-${TF_RESOURCE_NAME}-aws-alb-ingress-controller"
-    path        = "/"
-    description = "Policy for K8s AWS ALB Ingress Controller"
-
-    policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [
-        "acm:DescribeCertificate",
-        "acm:ListCertificates",
-        "acm:GetCertificate"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:AuthorizeSecurityGroupIngress",
-        "ec2:CreateSecurityGroup",
-        "ec2:CreateTags",
-        "ec2:DeleteTags",
-        "ec2:DeleteSecurityGroup",
-        "ec2:DescribeInstances",
-        "ec2:DescribeInstanceStatus",
-        "ec2:DescribeSecurityGroups",
-        "ec2:DescribeSubnets",
-        "ec2:DescribeTags",
-        "ec2:DescribeVpcs",
-        "ec2:ModifyInstanceAttribute",
-        "ec2:ModifyNetworkInterfaceAttribute",
-        "ec2:RevokeSecurityGroupIngress"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "elasticloadbalancing:AddTags",
-        "elasticloadbalancing:CreateListener",
-        "elasticloadbalancing:CreateLoadBalancer",
-        "elasticloadbalancing:CreateRule",
-        "elasticloadbalancing:CreateTargetGroup",
-        "elasticloadbalancing:DeleteListener",
-        "elasticloadbalancing:DeleteLoadBalancer",
-        "elasticloadbalancing:DeleteRule",
-        "elasticloadbalancing:DeleteTargetGroup",
-        "elasticloadbalancing:DeregisterTargets",
-        "elasticloadbalancing:DescribeListeners",
-        "elasticloadbalancing:DescribeLoadBalancers",
-        "elasticloadbalancing:DescribeLoadBalancerAttributes",
-        "elasticloadbalancing:DescribeRules",
-        "elasticloadbalancing:DescribeSSLPolicies",
-        "elasticloadbalancing:DescribeTags",
-        "elasticloadbalancing:DescribeTargetGroups",
-        "elasticloadbalancing:DescribeTargetGroupAttributes",
-        "elasticloadbalancing:DescribeTargetHealth",
-        "elasticloadbalancing:ModifyListener",
-        "elasticloadbalancing:ModifyLoadBalancerAttributes",
-        "elasticloadbalancing:ModifyRule",
-        "elasticloadbalancing:ModifyTargetGroup",
-        "elasticloadbalancing:ModifyTargetGroupAttributes",
-        "elasticloadbalancing:RegisterTargets",
-        "elasticloadbalancing:RemoveTags",
-        "elasticloadbalancing:SetIpAddressType",
-        "elasticloadbalancing:SetSecurityGroups",
-        "elasticloadbalancing:SetSubnets",
-        "elasticloadbalancing:SetWebACL"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "iam:GetServerCertificate",
-        "iam:ListServerCertificates"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "waf-regional:GetWebACLForResource",
-        "waf-regional:GetWebACL",
-        "waf-regional:AssociateWebACL",
-        "waf-regional:DisassociateWebACL"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "tag:GetResources",
-        "tag:TagResources"
-      ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "waf:GetWebACL"
-              ],
-      "Resource": "*"
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ChangeResourceRecordSets"
-      ],
-      "Resource": [
-        "arn:aws:route53:::hostedzone/*"
-      ]
-    },
-    {
-      "Effect": "Allow",
-      "Action": [
-        "route53:ListHostedZones",
-        "route53:ListResourceRecordSets"
-      ],
-      "Resource": [
-        "*"
-      ]
-    }
-  ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy_attachment" "ingress-attach" {
-    role       = "\${aws_iam_role.nodes-${TF_RESOURCE_NAME}.name}"
-    policy_arn = "\${aws_iam_policy.nodes-${TF_RESOURCE_NAME}-aws-alb-ingress-controller.arn}"
-}
-BASHEOF
-
-}
-
 apply_cluster_autoscaler_tf() {
     set_tf_resource_name
     # we can now specify the exact ASG instead of "*" for the autoscaler policy
@@ -409,11 +263,9 @@ install_namespaces() {
     fi
 }
 
-install_ingress_controller() {
-    echo "Installing ingress controller"
+install_elb_service() {
+    echo "Setting up LoadBalancer service"
 
-    kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.0/docs/examples/rbac-role.yaml
-    apply_ingress_controller_tf
     (cd "${KOPS_INSTALLER}/services/cluster-lb" && make deploy)
 }
 
@@ -442,7 +294,7 @@ usage() {
     echo "  ark                     install ark/velero for backups"
     echo "  metrics-server          install metrics-server"
     echo "  fluentd                 install fluentd"
-    echo "  ingress_controller      install ingress controller"
+    echo "  elb_instance            install ELB instance"
     echo "  all                     install all of the above components"
 }
 
@@ -468,8 +320,8 @@ if [ $# -eq 1 ]; then
             install_fluentd;;
         namespaces)
             install_namespaces;;
-        ingress_controller)
-            install_ingress_controller;;
+        elb_service)
+            install_elb_service;;
         all)
             install_all;;
         -h|--help)
