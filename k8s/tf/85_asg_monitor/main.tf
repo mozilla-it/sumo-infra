1fc746e50a859944742f2f9b58bfb61777b48f4d
eziegenhorn <eric@ziegenhorns.com>
2019-10-30 15:50:26 -0500

    Monitor ASGs in us-west-2a

---
+++ k8s/tf/85_asg_monitor/main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "us-west-2"
+  version = "~> 2"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sumo-state-095732026120"
+    key    = "terraform/sumo-infra-sns-asg"
+    region = "us-west-2"
+  }
+}
+
+variable "slack_webhook_url" {}
+
+module "monitor_asgs" {
+  source = "github.com/mozilla-it/asg-alert?ref=tags/1.0"
+  slack_webhook_url = "${var.slack_webhook_url}"
+  slack_channel     = "it-sre-bot"
+  slack_username    = "AWS"
+  asgs              = ["nodes.k8s.us-west-2a.sumo.mozit.cloud", "master-us-west-2a.masters.k8s.us-west-2a.sumo.mozit.cloud"]
+}
