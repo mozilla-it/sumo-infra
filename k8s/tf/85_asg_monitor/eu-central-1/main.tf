101d0d5d5cbb500b6f3bf3c7a1aee1bef045001f
eziegenhorn <eric@ziegenhorns.com>
2019-10-30 15:53:19 -0500

    Split ASG checks by region

--- k8s/tf/85_asg_monitor/eu-central-1/main.tf
+++ k8s/tf/85_asg_monitor/eu-central-1/main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region  = "us-west-2"
+  region  = "eu-centraul-1"
   version = "~> 2"
 }
 
 terraform {
   backend "s3" {
     bucket = "sumo-state-095732026120"
-    key    = "terraform/sumo-infra-sns-asg"
+    key    = "terraform/sumo-infra-sns-asg-eu"
     region = "us-west-2"
   }
 }
@@ -18,5 +18,5 @@ module "monitor_asgs" {
   slack_webhook_url = "${var.slack_webhook_url}"
   slack_channel     = "it-sre-bot"
   slack_username    = "AWS"
-  asgs              = ["nodes.k8s.us-west-2a.sumo.mozit.cloud", "master-us-west-2a.masters.k8s.us-west-2a.sumo.mozit.cloud"]
+  asgs              = ["nodes.k8s.eu-central-1a.sumo.mozit.cloud", "master-eu-central-1a.masters.k8s.eu-central-1a.sumo.mozit.cloud"]
 }
