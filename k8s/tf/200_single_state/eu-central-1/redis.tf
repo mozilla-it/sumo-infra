b42c948f717e11c54eeeefdfe324c8f8bba867f6
Alberto del Barrio <adelbarrio@mozilla.com>
2020-10-27 16:31:50 +0100

    Place Redis clusters in the right subnet and security group

--- k8s/tf/200_single_state/eu-central-1/redis.tf
+++ k8s/tf/200_single_state/eu-central-1/redis.tf
@@ -1,10 +1,12 @@
 resource "aws_elasticache_cluster" "dev" {
-  cluster_id      = "sumo-redis-dev-env"
-  engine          = "redis"
-  node_type       = "cache.t3.micro"
-  num_cache_nodes = 1
-  engine_version  = "5.0.5"
-  port            = 6379
+  cluster_id         = "sumo-redis-dev-env"
+  engine             = "redis"
+  node_type          = "cache.t3.micro"
+  num_cache_nodes    = 1
+  engine_version     = "5.0.5"
+  port               = 6379
+  subnet_group_name  = "sumo-backup"
+  security_group_ids = [data.aws_security_group.nodes_k8s_eu-central-1a_sumo_mozit_cloud.id]
   tags = {
     "environment" = "dev",
     "application" = "sumo"
@@ -12,12 +14,14 @@ resource "aws_elasticache_cluster" "dev" {
 }
 
 resource "aws_elasticache_cluster" "test" {
-  cluster_id      = "sumo-redis-test-env"
-  engine          = "redis"
-  node_type       = "cache.t3.micro"
-  num_cache_nodes = 1
-  engine_version  = "5.0.5"
-  port            = 6379
+  cluster_id         = "sumo-redis-test-env"
+  engine             = "redis"
+  node_type          = "cache.t3.micro"
+  num_cache_nodes    = 1
+  engine_version     = "5.0.5"
+  port               = 6379
+  subnet_group_name  = "sumo-backup"
+  security_group_ids = [data.aws_security_group.nodes_k8s_eu-central-1a_sumo_mozit_cloud.id]
   tags = {
     "environment" = "test",
     "application" = "sumo"
