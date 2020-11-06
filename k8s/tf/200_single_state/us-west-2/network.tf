760c149eda6bdd5c37313a048f31ccf4fa11d6de
Alberto del Barrio <adelbarrio@mozilla.com>
2020-11-06 15:50:28 +0100

    Create EKS cluster in us-west-2

---
+++ k8s/tf/200_single_state/us-west-2/network.tf
@@ -0,0 +1,3 @@
+data "aws_security_group" "nodes_k8s_us-west-2a_sumo_mozit_cloud" {
+  id = "sg-0fde630fb730ee9ed"
+}
---
+++ k8s/tf/200_single_state/us-west-2/state.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 0.12"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sumo-state-095732026120"
+    key    = "terraform/us-west-2/terraform.tfstate"
+    region = "us-west-2"
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
