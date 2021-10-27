3e1a2424dc4a3df56cd3d9742405dde3be8f628f
Tasos Katsoulas <akatsoulas@gmail.com>
2021-10-27 20:40:22 +0300

    S3 and CDN for Pocket help center.

---
+++ k8s/tf/120_pocket_support/main.tf
@@ -0,0 +1,98 @@
+
+provider "aws" {
+  region  = var.region
+  version = ">= 0.12"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sumo-state-095732026120"
+    key    = "terraform/pocket-helpcenter"
+    region = "us-west-2"
+  }
+}
+
+resource "aws_s3_bucket" "pocket_helpcenter" {
+  acl = "public-read"
+
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["GET"]
+    allowed_origins = ["*"]
+    max_age_seconds = 3000
+  }
+
+}
+
+data "aws_acm_certificate" "cert" {
+  domain = "*.sumo.mozit.cloud"
+}
+
+resource "aws_cloudfront_distribution" "pocket_helpcenter" {
+
+  comment         = "Pocket Help Center distribution"
+  enabled         = true
+  http_version    = "http2"
+  is_ipv6_enabled = false
+  price_class     = "PriceClass_All"
+
+  origin {
+    origin_id   = "pocket-helpcenter"
+    domain_name = aws_s3_bucket.pocket_helpcenter.bucket_regional_domain_name
+
+    custom_origin_config {
+      http_port                = "80"
+      https_port               = "443"
+      origin_protocol_policy   = "https-only"
+      origin_read_timeout      = 30
+      origin_ssl_protocols     = ["TLSv1", "TLSv1.1", "TLSv1.2"]
+      origin_keepalive_timeout = 5
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    compress         = true
+    default_ttl      = 3600
+    max_ttl          = 86400
+    min_ttl          = 0
+    target_origin_id = "pocket-helpcenter"
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    viewer_protocol_policy = "https-only"
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = data.aws_acm_certificate.cert.arn
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1"
+  }
+}
+
+data "aws_route53_zone" "sumo_mozit_cloud" {
+  name = "sumo.mozit.cloud"
+}
+
+resource "aws_route53_record" "pocket_helpcenter" {
+  zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
+  name    = "pocket-helpcenter.sumo.mozit.cloud"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.pocket_helpcenter.domain_name
+    zone_id                = data.aws_route53_zone.sumo_mozit_cloud.zone_id
+    evaluate_target_health = true
+  }
+}
+
