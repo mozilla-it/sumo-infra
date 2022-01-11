4e5159e28023bae09b5a4c9a17799b247e23a431
Rogerio Varalda <rvaralda@mozilla.com>
2022-01-11 11:40:33 +0100

    Remove EU stage DNS as CNAME

--- k8s/tf/200_single_state/us-west-2/redis.tf
+++ k8s/tf/200_single_state/us-west-2/redis.tf
@@ -1,11 +1,11 @@
 resource "aws_elasticache_cluster" "stage" {
-  cluster_id         = "sumo-redis-stage-env"
-  engine             = "redis"
-  node_type          = "cache.t3.micro"
-  num_cache_nodes    = 1
-  engine_version     = "5.0.5"
-  port               = 6379
-  subnet_group_name  = "sumo-prod"
+  cluster_id        = "sumo-redis-stage-env"
+  engine            = "redis"
+  node_type         = "cache.t3.micro"
+  num_cache_nodes   = 1
+  engine_version    = "5.0.5"
+  port              = 6379
+  subnet_group_name = "sumo-prod"
   security_group_ids = [
     data.aws_security_group.nodes_k8s_us-west-2b_sumo_mozit_cloud.id,
     data.aws_security_group.nodes_k8s_us-west-2a_sumo_mozit_cloud.id,
