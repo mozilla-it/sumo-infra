92a45916bf4184f5a521c9354fc193062bf4036a
Rogerio Varalda <rvaralda@mozilla.com>
2022-01-11 11:31:25 +0100

    Stage update: add EKS SG to redis in EU and US; Update EKS nodes instace type to m5.xlarge

--- k8s/tf/200_single_state/eu-central-1/dns.tf
+++ k8s/tf/200_single_state/eu-central-1/dns.tf
@@ -3,13 +3,33 @@ data "aws_route53_zone" "sumo_mozit_cloud" {
 }
 
 data "aws_elb" "stage_frankfurt_sumo" {
-  name = "a9777a701750711eba90202d989fd40b"
+  name = "a13fb08e840fd4f16bbd518816275392"
 }
 
 resource "aws_route53_record" "stage-frankfurt_sumo_mozit_cloud" {
   zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
   name    = "stage-frankfurt.sumo.mozit.cloud"
-  type    = "CNAME"
-  ttl     = "300"
-  records = [data.aws_elb.stage_frankfurt_sumo.dns_name]
+  type    = "A"
+
+  alias {
+    name                   = data.aws_elb.stage_frankfurt_sumo.dns_name
+    zone_id                = data.aws_elb.stage_frankfurt_sumo.zone_id
+    evaluate_target_health = true
+  }
+}
+
+data "aws_elb" "dev_sumo" {
+  name = "a5a01e97041b042e5a2e14f28c831c44"
+}
+
+resource "aws_route53_record" "dev_sumo_mozit_cloud" {
+  zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
+  name    = "dev.sumo.mozit.cloud"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_elb.dev_sumo.dns_name
+    zone_id                = data.aws_elb.dev_sumo.zone_id
+    evaluate_target_health = true
+  }
 }
--- k8s/tf/200_single_state/us-west-2/network.tf
+++ k8s/tf/200_single_state/us-west-2/network.tf
@@ -5,3 +5,7 @@ data "aws_security_group" "nodes_k8s_us-west-2a_sumo_mozit_cloud" {
 data "aws_security_group" "nodes_k8s_us-west-2b_sumo_mozit_cloud" {
   id = "sg-05f3b5d24b59143e9"
 }
+
+data "aws_security_group" "sumo-eks-us-west-2" {
+  id = "sg-0b5c332983a11887c"
+}
