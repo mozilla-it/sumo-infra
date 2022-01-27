d6dab1e67c8f5ed5ce80ceab1ffef6f6cd63b838
Rogerio Varalda <rvaralda@mozilla.com>
2022-01-27 17:55:53 +0100

    update eks us-west-2

--- k8s/tf/200_single_state/us-west-2/traffic-policy.tf
+++ k8s/tf/200_single_state/us-west-2/traffic-policy.tf
@@ -8,7 +8,7 @@ resource "aws_route53_health_check" "stage" {
   enable_sni        = true
 
   tags = {
-    Name = "SUMO Stage us-west-2 EKS"
+    Name = "Sumo Stage us-west-2 EKS"
   }
 }
 
@@ -32,3 +32,39 @@ resource "aws_cloudwatch_metric_alarm" "stage_lb_dns_name" {
     HealthCheckId = aws_route53_health_check.stage.id
   }
 }
+
+# to be done after migration
+#resource "aws_route53_health_check" "prod" {
+#  fqdn              = "prod-oregon.sumo.mozit.cloud"
+#  port              = 443
+#  type              = "HTTPS"
+#  resource_path     = "/readiness/"
+#  failure_threshold = "3"
+#  request_interval  = "30"
+#  enable_sni        = true
+#
+#  tags = {
+#    Name = "Sumo Prod us-west-2 EKS"
+#  }
+#}
+#
+#resource "aws_cloudwatch_metric_alarm" "prod_lb_dns_name" {
+#  provider            = aws.us-east-1
+#  alarm_name          = "Sumo_Prod_us-west-2-awsroute53"
+#  comparison_operator = "LessThanThreshold"
+#  evaluation_periods  = "1"
+#  metric_name         = "HealthCheckStatus"
+#  namespace           = "AWS/Route53"
+#  period              = "60"
+#  statistic           = "Minimum"
+#  threshold           = "1"
+#  treat_missing_data  = "missing"
+#  alarm_description   = "Monitor Prod EKS us-west-2 health"
+#
+#  actions_enabled = "true"
+#  alarm_actions   = ["arn:aws:sns:us-east-1:095732026120:MozillaSumoSlack"]
+#
+#  dimensions = {
+#    HealthCheckId = aws_route53_health_check.prod.id
+#  }
+#}
