78785c19e3d340a22a004875a64cbe214d83c6d7
Rogerio Varalda <rvaralda@mozilla.com>
2022-01-31 10:08:22 +0100

    Update DNS for prod EU

--- k8s/tf/200_single_state/eu-central-1/dns.tf
+++ k8s/tf/200_single_state/eu-central-1/dns.tf
@@ -2,6 +2,18 @@ data "aws_route53_zone" "sumo_mozit_cloud" {
   name = "sumo.mozit.cloud"
 }
 
+data "aws_elb" "prod_frankfurt_sumo" {
+  name = "a82560fd24f254c73aa0eb25dd537be6"
+}
+
+resource "aws_route53_record" "prod-frankfurt_sumo_mozit_cloud" {
+  zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
+  name    = "prod-frankfurt.sumo.mozit.cloud"
+  type    = "CNAME"
+  ttl     = "300"
+  records = [data.aws_elb.prod_frankfurt_sumo.dns_name]
+}
+
 data "aws_elb" "stage_frankfurt_sumo" {
   name = "a13fb08e840fd4f16bbd518816275392"
 }
