f48f688aa84cb7f09df161f929f0c36df464bc4a
Rogerio Varalda <rvaralda@mozilla.com>
2022-02-14 11:36:30 +0100

    Update the config after the DNS config applied

--- k8s/tf/200_single_state/us-west-2/dns.tf
+++ k8s/tf/200_single_state/us-west-2/dns.tf
@@ -2,18 +2,17 @@ data "aws_route53_zone" "sumo_mozit_cloud" {
   name = "sumo.mozit.cloud"
 }
 
-# to be applied after EKS migration
-# data "aws_elb" "prod_oregon_sumo" {
-#   name = "afcd59e18d6334565b2eb1e439217b36"
-# }
+data "aws_elb" "prod_oregon_sumo" {
+  name = "afcd59e18d6334565b2eb1e439217b36"
+}
 
-# resource "aws_route53_record" "prod-oregon_sumo_mozit_cloud" {
-#   zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
-#   name    = "prod-oregon.sumo.mozit.cloud"
-#   type    = "CNAME"
-#   ttl     = "300"
-#   records = [data.aws_elb.prod_oregon_sumo.dns_name]
-# }
+resource "aws_route53_record" "prod-oregon_sumo_mozit_cloud" {
+  zone_id = data.aws_route53_zone.sumo_mozit_cloud.zone_id
+  name    = "prod-oregon.sumo.mozit.cloud"
+  type    = "CNAME"
+  ttl     = "60"
+  records = [data.aws_elb.prod_oregon_sumo.dns_name]
+}
 
 data "aws_elb" "stage_sumo" {
   name = "af66a7e87cd014b2e8a364f2ee250402"
