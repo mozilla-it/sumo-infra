39a0382e1ba7e72a7e49ddd7934f67f006bafdb6
Alan <duallain@gmail.com>
2022-02-28 09:04:51 -0800

    SE-3042 - Change prod rds to 31 days
    Per our normal retention policy, keep backups for at least 30 days
    using 31 to ensure we always have 30.

--- k8s/tf/70_prod_db_redis/main.tf
+++ k8s/tf/70_prod_db_redis/main.tf
@@ -20,7 +20,7 @@ module "mysql-prod" {
   mysql_password                    = var.mysql_prod_password
   mysql_identifier                  = "sumo"
   mysql_instance_class              = "db.m5.4xlarge"
-  mysql_backup_retention_days       = 14
+  mysql_backup_retention_days       = 31
   mysql_security_group_name         = "sumo_rds_sg_prod"
   mysql_storage_gb                  = 250
   mysql_storage_type                = "gp2"
