7a03d112a00223e5f6106e1e4c41c4eca71796c4
Rogerio Varalda <rvaralda@mozilla.com>
2022-07-29 14:12:00 +0200

    Oregon EKS terraform sync up

--- k8s/tf/200_eks/us-west-2/eks.tf
+++ k8s/tf/200_eks/us-west-2/eks.tf
@@ -34,8 +34,9 @@ module "eks-us-west-2" {
     "arn:aws:iam::517826968395:role/itsre-admin",
     "arn:aws:iam::095732026120:role/maws-sumo-poweruser"
   ]
-  region      = "us-west-2"
-  node_groups = local.node_groups
+  region         = "us-west-2"
+  node_groups    = local.node_groups
+  enable_logging = true
 
   velero_settings = {
     "initContainers[0].image"                             = "velero/velero-plugin-for-aws:v1.3.0"
