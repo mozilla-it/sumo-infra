cd8e68150d384c08c76d17b3ba05ec0ed5f791d2
André Honeiser <ahoneiser@mozilla.com>
2023-06-02 14:54:30 +0200

    chore(tf): drop unused route53 zones

--- k8s/tf/01_dns/outputs.tf
+++ k8s/tf/01_dns/outputs.tf
@@ -10,18 +10,6 @@ output "master-zone" {
   value = element(concat(aws_route53_zone.master-zone.*.zone_id, [""]), 0)
 }
 
-output "us-west-2-zone-id" {
-  value = module.us-west-2.hosted_zone_id
-}
-
-output "us-west-2a-zone-id" {
-  value = module.us-west-2a.hosted_zone_id
-}
-
-output "us-west-2b-zone-id" {
-  value = module.us-west-2b.hosted_zone_id
-}
-
 output "cdn-zone-id" {
   value = aws_route53_zone.cdn.zone_id
 }
